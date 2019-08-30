44576a7def013b8258f37746c6d35c8096e30534
aptsaous <apostolos.tsaousis@icloud.com>
2019-08-31 02:38:30 +0300

    Initial commit

---
+++ src/main/java/gr/efood/foodsearcher/FoodSearcherApplication.java
@@ -0,0 +1,13 @@
+package gr.efood.foodsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/efood/foodsearcher/FoodSearcherApplicationTests.java
@@ -0,0 +1,16 @@
+package gr.efood.foodsearcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodSearcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
